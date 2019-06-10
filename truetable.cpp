cb0c308898074aca9b79bd7d7c5c04b44a3c18bb
Yimaha <justin@timesaved.ca>
2019-06-10 17:07:45 -0400

    update to final version

--- truetable.cpp
+++ truetable.cpp
@@ -1,24 +1,60 @@
 #include<iostream>
 #include<string>
 #include<algorithm>
-#include<math.h>   
+#include<math.h>
 using namespace std;
 
 int main(int argc, char* argv[]){
     //first identify type of boolean
     std::string input = argv[1];
     //if And Gate
-    if(input.find("*") != std::string::npos){
+    if(input[1] == '.'){
         //number of input
-        int n = std::count(input.begin(), input.end(), '*') + 1;
+        int n = std::count(input.begin(), input.end(), '.') + 1;
         //array of all the input
         std::string inputArray[n];
-        //
-        int pos = input.find("*");
+        int pos = input.find('.');
         for(int i = 0; i < n; i++){
             inputArray[i] = input.substr(0, pos);
             input.erase(0, pos + 1);
-            pos = input.find("*");
+            pos = input.find('.');
+        }
+
+        for(int i = 0; i< n; i++){
+            std::cout << inputArray[i] << " ";
+        }
+        std::cout << argv[1] << std::endl;
+
+        int end = pow(2, n);
+    for(int i=0; i< end; i++){
+        char output[n];
+        std::fill(output, output + n, 'F');
+        int current = i;
+        for(int j = n-1; j > -1; j--){
+            if(current >= pow(2, j)){
+            current = current - pow(2,j);
+            output[j] = 'T';
+            }
+        }
+        for(int j = 0; j < n; j++){
+            std::cout << output[j] << " ";
+        }
+        if(i == end -1)
+        std::cout << "T" << std::endl;
+        else
+        std::cout << "F" << std::endl;
+        }
+    } else {
+        //number of input
+        int n = std::count(input.begin(), input.end(), '+') + 1;
+        //array of all the input
+        std::string inputArray[n];
+        //
+        int pos = input.find("+");
+        for(int i = 0; i < n; i++){
+            inputArray[i] = input.substr(0, pos);
+            input.erase(0, pos + 1);
+            pos = input.find("+");
         }
 
         for(int i = 0; i< n; i++){
@@ -29,29 +65,23 @@ int main(int argc, char* argv[]){
         int end = pow(2, n);
 
         for(int i=0; i< end; i++){
-            int output[n].fill(0);
+            char output[n];
+            std::fill(output, output + n, 'F');
             int current = i;
-            for(int j = n-1; j > -1; j++){
-                if(current > pow(2, j))
-                current = current - pow(2,j)
-                output[j] = 1
+            for(int j = n-1; j > -1; j--){
+                if(current >= pow(2, j)){
+                current = current - pow(2,j);
+                output[j] = 'T';
+                }
             }
             for(int j = 0; j < n; j++){
                 std::cout << output[j] << " ";
             }
-            if(i == end -1)
-            std::cout << "1" << std::endl;
+            if(i == 0)
+            std::cout << "F" << std::endl;
             else
-            std::cout << "0" << std::endl;
+            std::cout << "T" << std::endl;
+
         }
-        
-    } else {
-        //if OR Gate
     }
-    int i;
-    cout << "argc ="<< argc << endl;
-    for (i = 0; i < argc; i++) {
-        cout << "argv[" << i << "] ="  << argv[i] << endl;
-    }
-    cout << endl;
 }
